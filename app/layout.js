e1e1cb275ad2ef5e17bb85b207b1622a4491d4cb
Daniel <danielchanghong1610@gmail.com>
2026-05-18 17:52:18 +0800

    Remove dark mode

--- app/layout.js
+++ app/layout.js
@@ -5,13 +5,10 @@ export const metadata = {
   description: "A beautiful and simple rent tracking application.",
 };
 
-import ThemeToggle from "./ThemeToggle";
-
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body>
-        <ThemeToggle />
         {children}
       </body>
     </html>
